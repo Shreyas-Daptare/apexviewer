964c1b6d55d05a436c9ec5eca5e0d4d1112e8dd3
Shreyas Daptare <116744812+Shreyas-Daptare@users.noreply.github.com>
2023-04-01 12:51:09 +0530

    apexviewer v4.0 Final Release
    Removed Unnecessary Dependencies, reduced actuall lines of executable code to improve performance and Comments were added

--- apexviewer.java
+++ apexviewer.java
@@ -1,92 +1,107 @@
 //-MADE WITH Little Programming Knowledge and Help from Internet
-//-By ZINCop
+//-By ZINCop (https://github.com/Shreyas-Daptare)
+// Explanation is added at the end, refer them using Comment_id
 // Modern Problems Require Modern Solutions
+
 import java.applet.Applet;
 import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Graphics;
-import java.awt.Panel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.net.URL;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JOptionPane;
-import java.net.URL;
 import java.net.URLClassLoader;
 
+public class apexviewer extends JFrame {
 
-public class apexviewer extends JFrame implements ActionListener {
-
+    //[Comment_id: 0] Serial Version UID to identify the class during deserialization
     private static final long serialVersionUID = 1L;
+
+    // Applet instance variable
     private Applet applet;
-    private Button loadButton;
 
-    public apexviewer() {
-        super("Applet Viewer");
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+    // Constructor
+    public apexviewer(String classLocation) {
+        super("Applet Viewer"); // Set window title
+        setSize(800, 600); // Set window size
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set default close operation
+        setLayout(new BorderLayout()); // Set layout of window
 
-        loadButton = new Button("Load Applet");
-        loadButton.addActionListener(this);
-        Panel buttonPanel = new Panel();
-        buttonPanel.add(loadButton);
-        add(buttonPanel, BorderLayout.NORTH);
+        try {
+            //[Comment_id: 1]
+            // Create a file object from the given class location
+            File file = new File(classLocation);
 
+            // Convert file object to URL object
+            URL url = file.toURI().toURL();
+
+            // Create a new instance of URLClassLoader with the URL of the file
+            ClassLoader classLoader = new URLClassLoader(new URL[] { url });
+
+            // Get the name of the applet class from the file name (without the extension)
+            String className = file.getName().split("\\.")[0];
+
+            // Load the applet class using the classLoader and create a new instance of it
+            applet = (Applet) classLoader.loadClass(className).newInstance();
+
+            // Add the applet to the center of the window
+            add(applet, BorderLayout.CENTER);
+
+            // Validate the window (to ensure that all components are properly laid out)
+            validate();
+
+            // Initialize the applet
+            applet.init();
+
+            // Start the applet
+            applet.start();
+
+        } catch (ClassNotFoundException ex) {
+            // If the class is not found, show an error message dialog
+            JOptionPane.showMessageDialog(this, "Class not found: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (Exception ex) {
+            // If any other exception occurs, show an error message dialog
+            JOptionPane.showMessageDialog(this, "Exception occurred: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        // Set the window to be visible
         setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == loadButton) {
-            JFileChooser fileChooser = new JFileChooser();
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("Applet Class", "class");
-            fileChooser.setFileFilter(filter);
-            int result = fileChooser.showOpenDialog(this);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
-                try {
-                    URL url = file.toURI().toURL();
-                    String className = file.getName().split("\\.")[0];
-                    ClassLoader classLoader = new URLClassLoader(new URL[] {url});
-                    applet = (Applet) classLoader.loadClass(className).newInstance();
-                    add(applet, BorderLayout.CENTER);
-                    validate();
-                    applet.init();
-                    applet.start();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                    JOptionPane.showMessageDialog(this, "Error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        }
-    }
-
+    // Main method
     public static void main(String[] args) {
         if (args.length == 0) {
+            // If no arguments are provided, print the usage message and return
             System.out.println("Usage: java apexviewer [class_location]");
             return;
         }
-        new apexviewer(args[0]);
-    }
 
-    public apexviewer(String classLocation) {
-        this();
-        try {
-            File file = new File(classLocation);
-            URL url = file.toURI().toURL();
-            ClassLoader classLoader = new URLClassLoader(new URL[] {url});
-            applet = (Applet) classLoader.loadClass(file.getName().split("\\.")[0]).newInstance();
-            add(applet, BorderLayout.CENTER);
-            validate();
-            applet.init();
-            applet.start();
-        } catch (ClassNotFoundException ex) {
-            JOptionPane.showMessageDialog(this, "Class not found: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(this, "Exception occurred: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-        }
+        // Create a new instance of apexviewer with the absolute path of the file specified in the arguments
+        new apexviewer(new File(args[0]).getAbsolutePath());
     }
 }
+
+/*
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Simplified Code:
+
+Comment ID
+0:  This line declares a private static final field serialVersionUID of type long with a value of 1L.
+    This field is used to provide version control of serialized objects. It is a unique identifier for a class that implements the java.io.Serializable interface.
+    When an object is serialized, its class is identified by its serialVersionUID.
+    The serialVersionUID is used to ensure that the same version of the class is used to serialize and deserialize an object.
+    If the serialVersionUID of the serialized object does not match the serialVersionUID of the class in the receiving JVM, an InvalidClassException is thrown.
+
+    In Java, serialVersionUID is a unique identifier that is used during deserialization to verify that the sender and receiver of a serialized object have loaded classes for that object that are compatible with respect to serialization.
+    If the serialVersionUID values of the sender and receiver classes do not match, then a InvalidClassException is thrown at runtime.
+    Using a specific value for serialVersionUID ensures that different versions of the same class have different identifiers, even if the class definition changes.
+    In this case, the value 1L is likely used as a simple placeholder or default value, indicating that the class has a serial version UID of 1.
+
+1:  In this code block, the file object representing the location of the applet class is converted to a URL object so that it can be used to create a new instance of URLClassLoader.
+    URLClassLoader is a subclass of ClassLoader that is used to load classes from a URL.
+
+    Here, the file.toURI().toURL() method call is used to convert the file object to a URL object.
+    The resulting URL object is then passed as an argument to the constructor of URLClassLoader to create a new instance of URLClassLoader that can load classes from the specified URL.
+    So in summary, this code block is responsible for creating a ClassLoader that can load the applet class from the specified location.
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ */
